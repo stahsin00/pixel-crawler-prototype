078ae0c17d7043c7644c8cd8639a6c2f1b5ad969
stahsin00 <lamisa1998@hotmail.com>
2024-04-19 20:25:44 -0700

    changed algorithm to make main path

--- Assets/Scripts/World/Region.cs
+++ Assets/Scripts/World/Region.cs
@@ -1,11 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// idr wtf is going on here but ill fix it eventually i swear
 public class Region
 {
-    public int type;
+    public int Type { get; private set; }
     public int worldX;
     public int worldY;
 
@@ -16,18 +14,14 @@ public class Region
     private List<Vector2Int> entrances;
     private List<Vector2Int> exits;
 
-    // T-T
-    private int[] mainPath1 = { 1, 2, 2, 3, 2, 4, 5, 6, 7 };
-    private int[] mainPath2 = { 1, 2, 2, 2, 3, 2, 4, 5, 6, 7 };
-    private int[] mainPath3 = { 1, 2, 2, 3, 2, 2, 4, 5, 6, 7 };
-    private int[] mainPath4 = { 1, 2, 2, 2, 3, 2, 2, 4, 5, 6, 7 };
+    private int[] mainPath = { 1, 2, 2, 3, 2, 4, 5, 6, 7 };
 
     public int collectibles;
     public int healthBoosts;
 
     public Region(int type, int worldX, int worldY, bool main = false, int collectibles = 0, int healthBoosts = 0)
     {
-        this.type = type;
+        this.Type = type;
         
         this.worldX = worldX;
         this.worldY = worldY;
@@ -63,96 +57,67 @@ public class Region
 
     public void Initialize()
     {
-        MakeMainPath();
+        MakePath();
 
-        // foreach (Vector2Int exit in exits)
-        // {
-        //     FindPath(exit);
-        // }
+        foreach (Vector2Int entrance in entrances)
+        {
+            FindPath(entrance, true);
+        }
 
-        // for (int i = 0; i < collectibles; i++)
-        // {
-        //     FindPath(0);
-        // }
+        foreach (Vector2Int exit in exits)
+        {
+            FindPath(exit);
+        }
 
-        // for (int i = 0; i < healthBoosts; i++)
-        // {
-        //     FindPath(1);
-        // }
+        for (int i = 0; i < collectibles; i++)
+        {
+            // TODO
+        }
+
+        for (int i = 0; i < healthBoosts; i++)
+        {
+            // TODO
+        }
     }
 
-    private void MakeMainPath()
+    private void MakePath()
     {
-        //MainPathBFS(entrance);
-        List<Vector2Int> path = MakeMainPathHelper(entrance, mainPath1.Length);
+        bool[,] visited = new bool[size, size];
+        MakePathDFS(entrance.x,entrance.y,visited,9);
 
-        foreach (Vector2Int position in path)
-        {
-            //Debug.Log($"Region {type} Position: ({position.x},{position.y})");
-            room[position.x,position.y] = 1;
-        }
-
-        room[entrance.x,entrance.y] = 1;
-
-        // for (int i = 0; i < size; i++){
-        //     string row = "";
-        //     for (int j = 0; j < size; j++) {
-        //         row += $"{room[i,j]} ";
-        //     }
-        //     Debug.Log(row);
-        // }
     }
 
-    private List<Vector2Int> MakeMainPathHelper(Vector2Int source, int max) {
-        int[,] dist = new int[size,size];
-        Vector2Int[,] prev = new Vector2Int[size,size];
-        PriorityQueue<Vector2Int> priority = new PriorityQueue<Vector2Int>();
+    private bool MakePathDFS(int x, int y, bool[,] visited, int dist) {
+        if (dist == 0) {
+            return true;
+        }
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                dist[i,j] = size * size * 2;
-                prev[i,j] = new Vector2Int(size, size);
-                priority.Enqueue(new Vector2Int(i,j), dist[i,j]);
+        if (visited[x,y]) {
+            return false;
+        }
+
+        visited[x,y] = true;
+
+        (int, int)[] moves = { (1, 0), (-1, 0), (0, 1), (0, -1) };
+        Utility.ShuffleArray(moves);
+
+        foreach ((int, int)move in moves) {
+            int nextX = x + move.Item1;
+            int nextY = y + move.Item2;
+
+            if (!isValidMove(nextX,nextY)) {
+                continue;
+            }
+
+            bool found = MakePathDFS(nextX, nextY, visited, dist-1);
+            if (found) {
+                room[x,y] = 1;
+                return true;
             }
         }
 
-        dist[source.x,source.y] = 0;
-        priority.UpdatePriority(source,0);
-
-        Vector2Int cur = source;
-
-        while (priority.Count > 0) {
-            cur = priority.Dequeue();
-
-            if (cur != source) {
-                Vector2Int prevNode = prev[cur.x,cur.y];
-                if (dist[prevNode.x,prevNode.y] == max - 1) {
-                    break;
-                }
-            }
-            
-            foreach (var (dx, dy) in new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) }) {
-                var newX = cur.x + dx;
-                var newY = cur.y + dy;
-                if (isValidMove(newX, newY, true) && (dist[newX, newY] > dist[cur.x, cur.y] + 1)) {
-                    dist[newX, newY] = dist[cur.x, cur.y] + 1;
-                    prev[newX, newY] = cur;
-                    priority.UpdatePriority(new Vector2Int(newX, newY), dist[newX, newY]);
-                }
-            }
-        }
-
-        List<Vector2Int> path = new List<Vector2Int>();
-
-        while (cur != source)
-        {
-            path.Add(cur);
-            cur = prev[cur.x,cur.y];
-        }
-
-        path.Reverse();
-
-        return path;
+        visited[x,y] = false;
+        return false;
     }
 
     private bool isValidMove(int x, int y, bool inclusive = false)
@@ -160,142 +125,7 @@ public class Region
         return (x >= 0) && (x < size) && (y >= 0) && (y < size) && (inclusive || room[x, y] == 0);
     }
 
-    // genuinely wtf
-    private void MainPathBFS(Vector2Int entrance)
-    {
-        Queue<List<(int, int)>> solution;
-        List<(int, int)> path;
-
-        int type = Random.Range(0, 4);
-        int index;
-
-        switch (type)
-        {
-            case 0:
-                solution = MainPathBFSHelper(entrance, mainPath1.Length);
-                index = Random.Range(0, solution.Count - 1);
-                path = solution.Dequeue();
-                while (index > 1)
-                {
-                    path = solution.Dequeue();
-                    index--;
-                }
-                for (int i = 0; i < path.Count; i++)
-                {
-                    room[path[i].Item1, path[i].Item2] = mainPath1[i];
-                }
-                break;
-            case 1:
-                solution = MainPathBFSHelper(entrance, mainPath2.Length);
-                index = Random.Range(0, solution.Count - 1);
-                path = solution.Dequeue();
-                while (index > 1)
-                {
-                    path = solution.Dequeue();
-                    index--;
-                }
-                for (int i = 0; i < path.Count; i++)
-                {
-                    room[path[i].Item1, path[i].Item2] = mainPath2[i];
-                }
-                break;
-            case 2:
-                solution = MainPathBFSHelper(entrance, mainPath3.Length);
-                index = Random.Range(0, solution.Count - 1);
-                path = solution.Dequeue();
-                while (index > 1)
-                {
-                    path = solution.Dequeue();
-                    index--;
-                }
-                for (int i = 0; i < path.Count; i++)
-                {
-                    room[path[i].Item1, path[i].Item2] = mainPath3[i];
-                }
-                break;
-            default:
-                solution = MainPathBFSHelper(entrance, mainPath3.Length);
-                index = Random.Range(0, solution.Count - 1);
-                path = solution.Dequeue();
-                while (index > 1)
-                {
-                    path = solution.Dequeue();
-                    index--;
-                }
-                for (int i = 0; i < path.Count; i++)
-                {
-                    room[path[i].Item1, path[i].Item2] = mainPath4[i];
-                }
-                break;
-        }
-    }
-
-    private Queue<List<(int, int)>> MainPathBFSHelper(Vector2Int entrance, int length)
-    {
-        Queue<List<(int, int)>> solution = new Queue<List<(int, int)>>();
-
-        List<(int, int)> current = new List<(int, int)>();
-        current.Add((entrance.x, entrance.y));
-        solution.Enqueue(current);
-
-        while (solution.Peek().Count + 1 <= length)
-        {
-            int x = current[current.Count - 1].Item1;
-            int y = current[current.Count - 1].Item2;
-
-            if (isValidMove(x - 1, y) && !current.Contains((x - 1, y)))
-            {
-                List<(int, int)> path = new List<(int, int)>();
-                foreach((int, int) position in current)
-                {
-                    path.Add(position);
-                }
-                path.Add((x - 1, y));
-                solution.Enqueue(path);
-            }
-
-            if (isValidMove(x + 1, y) && !current.Contains((x + 1, y)))
-            {
-                List<(int, int)> path = new List<(int, int)>();
-                foreach ((int, int) position in current)
-                {
-                    path.Add(position);
-                }
-                path.Add((x + 1, y));
-                solution.Enqueue(path);
-            }
-
-            if (isValidMove(x, y - 1) && !current.Contains((x, y - 1)))
-            {
-                List<(int, int)> path = new List<(int, int)>();
-                foreach ((int, int) position in current)
-                {
-                    path.Add(position);
-                }
-                path.Add((x, y - 1));
-                solution.Enqueue(path);
-            }
-
-            if (isValidMove(x, y + 1) && !current.Contains((x, y + 1)))
-            {
-                List<(int, int)> path = new List<(int, int)>();
-                foreach ((int, int) position in current)
-                {
-                    path.Add(position);
-                }
-                path.Add((x, y + 1));
-                solution.Enqueue(path);
-            }
-
-            if (solution.Peek().Count + 1 <= length) {
-                current = solution.Dequeue();
-            }
-        }
-
-        return solution;
-    }
-
-    private void FindPath(Vector2Int source) 
+    private void FindPath(Vector2Int source, bool isEntrance = false) 
     {
         List<Vector2Int> path = Utility.FindPath(size, source, isValidMove, (int x, int y) => { return room[x, y] > 0; });
 
@@ -304,11 +134,6 @@ public class Region
             room[position.x,position.y] = 2;
         }
 
-        room[source.x,source.y] = 8;
-    }
-
-    private void FindPath(int type)
-    {
-
+        room[source.x,source.y] = isEntrance ? 1 : 8;
     }
 }
