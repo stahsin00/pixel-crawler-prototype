bad0b4859d4b8afa183a2b8ba174c9569744f13f
stahsin00 <lamisa1998@hotmail.com>
2024-04-23 21:51:18 -0700

    make state, transition, and entity abstract

--- Assets/Scripts/StateMachine/Transition.cs
+++ Assets/Scripts/StateMachine/Transition.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transition
+public abstract class Transition
 {
     protected Entity entity;
     protected StateMachine stateMachine;
 
+    public Transition(Entity entity, StateMachine stateMachine)
+    {
+        this.entity = entity;
+        this.stateMachine = stateMachine;
+    }
+
     public virtual bool ShouldTransition()
     {
+        // TODO : temp
         return false;
     }
 
     public virtual State NextState()
     {
+        // TODO : temp
         return entity.idleState;
     }
 }
